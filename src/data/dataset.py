63bd61651cf771693b4958d8626405708699bdae
manel <manelbaradad@gmail.com>
2016-12-18 22:43:03 +0100

    Added missing init and partial submission.py

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -81,13 +81,3 @@ class Dataset():
             idxs = np.sort(idx_values[i * chunk_size:i * chunk_size + chunk_size])
             yield self.generate(idxs)
 
-
-if __name__ == "__main__":
-  d = Dataset()
-
-  start = time.clock()
-  i = 0
-  for j in d.generator(1):
-    print "time elapsed for processing image " + str(i) + ": " + str(time.clock() - start)
-    i += 1
-    start = time.clock()
