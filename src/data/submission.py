e00ca0f33c374e5816ae7a0f6a176b2ec7cebfbf
manel <manelbaradad@gmail.com>
2016-12-18 22:24:47 +0100

    Added missing init and partial submission.py

--- src/data/submission.py
+++ src/data/submission.py
@@ -2,13 +2,19 @@ from dataset import Dataset
 from classifiers.stubclassifier import StubClassifier
 
 def generate_submission(classifier, subset=-1):
-  d = Dataset(train=False)
 
-  classifier = StubClassifier()
-  image_list = d
-  for im in d:
+  d = Dataset(train=False, subset=subset)
+  classifier = StubClassifier(d)
+
+  for idx in range(len(d.get_image_list())):
+    im = d.generate_one(idx)
     masks = classifier.predict(d)
 
   # TODO: Maybe further reclassify polygons??
   # For example, classify between with a Decision tree on the area of the vehicle
   return None
+
+if __name__ == "__main__":
+  classifier = StubClassifier()
+  generate_submission(classifier, subset=1)
+
